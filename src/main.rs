08a51c356d2c30d5b364774020cab82ea6ca9ce3
Arnold <arnold@afach.de>
2024-03-07 15:51:05 +0400

    Support write and read for phone book

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,42 @@
-use std::{collections::BTreeMap, io::Write};
+use std::{collections::BTreeMap, fs::read, io::Write};
 
 fn main() {
     let mut phone_book = BTreeMap::new();
-    phone_book.insert("john", "0505555555");
-    phone_book.insert("mark", "0506666666");
-    phone_book.insert("jack", "0507777777");
+    phone_book.insert("john".to_string(), "0505555555".to_string());
+    phone_book.insert("mark".to_string(), "0506666666".to_string());
+    phone_book.insert("jack".to_string(), "0507777777".to_string());
 
 
     map_writer(phone_book, "file2.txt".to_string()).expect("failed to write");
+    let result = map_reader("file2.txt".to_string()).expect("Cannot read data");
+    println!("{:?}", result)
 }
 
 fn map_writer(
-    phone_book: BTreeMap<&str, &str>,
+    phone_book: BTreeMap<String, String>,
     file_path: String,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut file = std::fs::File::create(file_path)?;
-    for (name, phone_number) in phone_book {
+    for (i, (name, phone_number)) in phone_book.iter().enumerate() {
         file.write_all(name.as_bytes())?;
         file.write_all(": ".as_bytes())?;
         file.write_all(phone_number.as_bytes())?;
-        file.write_all("\n".as_bytes())?;
+        if i != phone_book.len() - 1 {
+            file.write_all("\n".as_bytes())?;
+        }
     }
     Ok(())
+}
+
+fn map_reader(file_path: String) -> Result<BTreeMap<String, String>, Box<dyn std::error::Error>> {
+    let r: Vec<u8> = read(file_path)?;
+    let x = String::from_utf8(r)?;
+    let x_split: Vec<&str> = x.split("\n").collect();
+
+    let mut strings = BTreeMap::new();
+    for word in x_split {
+        let word_split: Vec<&str> = word.split(": ").collect();
+        strings.insert(word_split[0].to_string(), word_split[1].to_string());
+    }
+    Ok(strings)
 }
