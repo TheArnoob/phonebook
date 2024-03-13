0e5df86d399843f8cc090c0faf1798b38dd3b89c
Arnold <arnold@afach.de>
2024-03-13 16:54:53 +0400

    Abstract user input with a function.

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,20 @@ const FILE_NAME: &str = "file.txt";
 fn main() {
     loop {
         println!("Enter one of these commands:");
-        println!("show, exit, add, remove, modify");
-        let mut commands = String::new();
-        std::io::stdin().read_line(&mut commands).unwrap();
-        let parsed_command = commands.trim().parse::<String>().unwrap();
-        if parsed_command == "show" {
+        let command = get_input_from_user("show, add, remove, modify, exit");
+        if command == "show" {
             let phone_book = map_reader(FILE_NAME.into()).expect("Cannot read data");
             println!("{phone_book:?}");
-        } else if parsed_command == "exit" {
+        } else if command == "exit" {
             return;
-        } else if parsed_command == "add" {
-            println!("please enter a name");
-            let mut name = String::new();
-            std::io::stdin().read_line(&mut name).unwrap();
-            println!("please enter a number");
-            let mut phone_number = String::new();
-            std::io::stdin().read_line(&mut phone_number).unwrap();
+        } else if command == "add" {
+            let name = get_input_from_user("Please enter a name");
+            let phone_number = get_input_from_user("Please enter a phone_number");
             let mut phone_book = map_reader(FILE_NAME.into()).expect("Cannot read data");
-            phone_book.insert(name.trim().to_string(), phone_number.trim().to_string());
+            phone_book.insert(name, phone_number);
             map_writer(phone_book, FILE_NAME.into()).expect("failed to write");
-        } else if parsed_command == "remove" {
-            println!("Please enter a name");
-            let mut name: String = String::new();
-            std::io::stdin().read_line(&mut name).unwrap();
-            let name = name.trim().to_string();
+        } else if command == "remove" {
+            let name = get_input_from_user("Please enter a name to remove");
             let mut phone_book = map_reader(FILE_NAME.into()).unwrap();
             if phone_book.contains_key(&name) {
                 phone_book.remove(&name);
@@ -37,16 +27,10 @@ fn main() {
             } else if !phone_book.contains_key(&name) {
                 println!("The file dosen't contain the data");
             }
-        } else if parsed_command == "modify" {
-            println!("Please enter a name to modify:");
-            let mut name = String::new();
-            std::io::stdin().read_line(&mut name).unwrap();
-            let name = name.trim().to_string();
+        } else if command == "modify" {
+            let name = get_input_from_user("Please enter a name to modify: ");
             let mut phone_book = map_reader(FILE_NAME.into()).unwrap();
-            println!("Please enter the new number: ");
-            let mut new_phone_number = String::new();
-            std::io::stdin().read_line(&mut new_phone_number).unwrap();
-            let new_phone_number = new_phone_number.trim().to_string();
+            let new_phone_number = get_input_from_user("please enter the new phone number");
             let mutable_entry = phone_book.get_mut(&name);
             match mutable_entry {
                 Some(phone_number_in_phone_book) => *phone_number_in_phone_book = new_phone_number,
@@ -76,7 +60,9 @@ fn map_writer(
     Ok(())
 }
 
-fn map_reader(file_path: std::path::PathBuf) -> Result<BTreeMap<String, String>, Box<dyn std::error::Error>> {
+fn map_reader(
+    file_path: std::path::PathBuf,
+) -> Result<BTreeMap<String, String>, Box<dyn std::error::Error>> {
     if !file_path.exists() {
         return Ok(BTreeMap::new());
     }
@@ -90,4 +76,12 @@ fn map_reader(file_path: std::path::PathBuf) -> Result<BTreeMap<String, String>,
         map.insert(word_split[0].to_string(), word_split[1].to_string());
     }
     Ok(map)
+}
+
+fn get_input_from_user(message: &str) -> String {
+    println!("{message}");
+    let mut name = String::new();
+    std::io::stdin().read_line(&mut name).unwrap();
+    let name = name.trim().to_string();
+    name
 }
