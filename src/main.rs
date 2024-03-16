d796980ec883ee380d8ef24a11aba5b899d8f883
Arnold <arnold@afach.de>
2024-03-16 12:35:22 +0400

    Add work number.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ const FILE_NAME: &str = "file.txt";
 #[derive(Debug)]
 struct PhoneEntry {
     mobile: String,
+    work: String
 }
 
-fn main() {
+fn main(){
     loop {
         println!("Enter one of these commands:");
         let command = get_input_from_user("show, add, remove, modify, exit");
@@ -19,11 +20,13 @@ fn main() {
         } else if command == "add" {
             let name = get_input_from_user("Please enter a name");
             let phone_number = get_input_from_user("Please enter a phone_number");
+            let phone_number1 = get_input_from_user("please enter another number");
             let mut phone_book = map_reader(FILE_NAME.into()).expect("Cannot read data");
             phone_book.insert(
                 name,
                 PhoneEntry {
                     mobile: phone_number,
+                    work: phone_number1
                 },
             );
             map_writer(phone_book, FILE_NAME.into()).expect("failed to write");
@@ -40,12 +43,14 @@ fn main() {
         } else if command == "modify" {
             let name = get_input_from_user("Please enter a name to modify: ");
             let mut phone_book = map_reader(FILE_NAME.into()).unwrap();
-            let new_phone_number = get_input_from_user("please enter the new phone number");
+            let new_phone_number = get_input_from_user("Please enter the new phone number");
+            let new_phone_number1 = get_input_from_user("Please enter another phone number");
             let mutable_entry = phone_book.get_mut(&name);
             match mutable_entry {
                 Some(phone_number_in_phone_book) => {
                     *phone_number_in_phone_book = PhoneEntry {
                         mobile: new_phone_number,
+                        work: new_phone_number1 
                     }
                 }
                 None => println!("the file dosen't contain the entry"),
@@ -66,6 +71,8 @@ fn map_writer(
         file.write_all(name.as_bytes())?;
         file.write_all(": ".as_bytes())?;
         file.write_all(phone_entry.mobile.as_bytes())?;
+        file.write_all(": ".as_bytes())?;
+        file.write_all(phone_entry.work.as_bytes())?;
         if i != phone_book.len() - 1 {
             file.write_all("\n".as_bytes())?;
         }
@@ -94,6 +101,7 @@ fn map_reader(
             word_split[0].to_string(),
             PhoneEntry {
                 mobile: word_split[1].to_string(),
+                work: word_split[2].to_string(),
             },
         );
     }
